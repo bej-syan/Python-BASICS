c272efc5d5289a11c3eac418dccaa6d261d5db37
bej-syan <all_bright@live.com>
2018-03-05 18:51:05 +0800

    Create CONCURRENCY.py

---
+++ CONCURRENCY.py
@@ -0,0 +1,29 @@
+# Speed It Up
+# Parallelize It
+# Life of a Thread
+# Synchronization between Threads
+# Communication between Threads
+# Debug and Benchmark
+# Executors and Pools
+# Multiprocessing
+# Event-Driven Programming
+# Reactive Programming
+# Using the GPU
+# Choosing a solution
+
+# W'LL USE THESE LIBRARIES/TOOLS/SOFTWARES:
+#   Beautiful Soup          // $ pip install beautifulsoup4
+#   RxPy                    // $ pip install rx
+#   Anaconda                // https://www.anaconda.com/download/
+#   Theano                  // $ pip install Theano
+#   PyOpenCL                // https://wiki.tiker.net/PyOpenCL/Installation/Windows
+
+# Getting the most out of your software is something all developers strive for, and concurrency, 
+# and the art of concurrent programming, happens to be one of the best ways in order for you to 
+# improve the performance of your applications. 
+
+# Gone are the days of unresponsive programs that give you no indication as to whether they’ve 
+# crashed or are still silently working.
+
+# By choosing to implement systems in a concurrent fashion, we typically see an increase in the
+# overall complexity of our code, and a heightened risk for bugs to appear within this new code.
